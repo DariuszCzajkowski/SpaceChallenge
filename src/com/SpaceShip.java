736629659c1a53da2a69a31f4ace676ef64329cd
Dariusz <czajk.dariusz@gmail.com>
2019-11-05 21:52:08 +0100

    config git ignore

--- src/com/SpaceShip.java
+++ src/com/SpaceShip.java
@@ -3,6 +3,6 @@ package com;
 public interface SpaceShip {
     boolean launch();
     boolean land();
-    boolean canCarry()   ;
-    int carry();
+    boolean canCarry(Item item)   ;
+    int carry(Item item);
 }
